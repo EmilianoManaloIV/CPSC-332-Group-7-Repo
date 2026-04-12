31218558f1f6560f4f632cc1f82a48487d17f7f1
zsalinasz <143377521+zsalinasz@users.noreply.github.com>
2026-04-12 12:18:18 -0700

    Add files via upload
    Added my education indicators table.

---
+++ education_indicators.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Education_Indicators (
+country_code 				CHAR(3),		
+education_id				INT				NOT NULL	AUTO_INCREMENT,
+year 						YEAR			NOT NULL,
+literacy_percentage     	DECIMAL(5,2)    DEFAULT NULL,
+primary_enrollment_pct  	DECIMAL(5,2)    DEFAULT NULL, 
+secondary_enrollment_pct	DECIMAL(5,2)    DEFAULT NULL, 
+education_gdp_pct			DECIMAL(5,2)    DEFAULT NULL,
+
+PRIMARY KEY (education_id),
+
+CONSTRAINT fk_education_country
+FOREIGN KEY (country_code) REFERENCES Country(Code)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
