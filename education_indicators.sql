11c466d54f03c8ea19cf8f674f3cddb2564e11ab
zsalinasz <143377521+zsalinasz@users.noreply.github.com>
2026-04-19 13:43:15 -0700

    Updated education_indicators.sql

--- education_indicators.sql
+++ education_indicators.sql
@@ -1,14 +1,14 @@
 CREATE TABLE Education_Indicators (
-country_code 				CHAR(3),		
-education_id				INT				NOT NULL	AUTO_INCREMENT,
-year 						YEAR			NOT NULL,
-literacy_percentage     	DECIMAL(5,2)    DEFAULT NULL,
-primary_enrollment_pct  	DECIMAL(5,2)    DEFAULT NULL, 
-secondary_enrollment_pct	DECIMAL(5,2)    DEFAULT NULL, 
-education_gdp_pct			DECIMAL(5,2)    DEFAULT NULL,
+    country_code              CHAR(3) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
+    education_id              INT NOT NULL AUTO_INCREMENT,
+    year                      YEAR NOT NULL,
+    literacy_percentage       DECIMAL(5,2) DEFAULT NULL,
+    primary_enrollment_pct    DECIMAL(5,2) DEFAULT NULL,
+    secondary_enrollment_pct  DECIMAL(5,2) DEFAULT NULL,
+    education_gdp_pct         DECIMAL(5,2) DEFAULT NULL,
 
-PRIMARY KEY (education_id),
+    PRIMARY KEY (education_id),
 
-CONSTRAINT fk_education_country
-FOREIGN KEY (country_code) REFERENCES Country(Code)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    CONSTRAINT fk_education_country
+        FOREIGN KEY (country_code) REFERENCES Country(Code)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
