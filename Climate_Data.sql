4521f3d05783c03e54314144a0e93819d5a7f06e
SassQuanch <sel810213@csu.fullerton.edu>
2026-05-04 16:22:57 -0700

    Replace drought_index with climate_stress_index

--- Climate_Data.sql
+++ Climate_Data.sql
@@ -3,8 +3,8 @@ CREATE TABLE Climate_Data (
     year YEAR NOT NULL,
     avg_temperature DECIMAL(4,1),
     avg_rainfall DECIMAL(8,2),
-    drought_index DECIMAL(5,2),
     co2_emissions DECIMAL(10,2),
+    climate_stress_index DECIMAL(5,2),
 
     PRIMARY KEY (country_code, year),
 
