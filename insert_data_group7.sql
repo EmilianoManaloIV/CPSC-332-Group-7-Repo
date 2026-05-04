c7d5e7fa81fe16e9bc5c58baf1c538f4db75489e
SassQuanch <sel810213@csu.fullerton.edu>
2026-05-03 18:24:01 -0700

    Update member list in insert_data_group7.sql

--- insert_data_group7.sql
+++ insert_data_group7.sql
@@ -3,7 +3,7 @@
 -- File: insert_data_group7.sql
 --       (rename: replace X with your group number)
 -- Group: Group7
--- Members: Emiliano, Zeus, Ava, Sean
+-- Members: Emiliano, Zeus, Ava, Sean Lowry
 -- Date: 04/08/2026
 -- ============================================
 
