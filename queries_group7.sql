9adcad89c2ca5405d3577a247a5fa05e9d2af52e
SassQuanch <sel810213@csu.fullerton.edu>
2026-05-03 18:24:44 -0700

    Update member name in queries_group7.sql
    Updated member name to include last name.

--- queries_group7.sql
+++ queries_group7.sql
@@ -3,7 +3,7 @@
 -- File: queries_group7.sql
 --       (rename: replace X with your group number)
 -- Group: Group7
--- Members: Zeus, Ava, Emiliano, Sean
+-- Members: Zeus, Ava, Emiliano, Sean Lowry
 -- Date: 04/08/2026
 -- ============================================
 
